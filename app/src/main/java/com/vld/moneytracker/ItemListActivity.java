e4220cfd85bcc5d0759850f6059662bd593951cd
Vladimir <vova435335>
2020-09-07 22:49:16 +0300

    Добавление цветов.

---
+++ app/src/main/java/com/vld/moneytracker/ItemListActivity.java
@@ -0,0 +1,15 @@
+package com.vld.moneytracker;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_items_list);
+    }
+}
